bd9016cace34fc677e5f46208f1b0bda95fb8687
Rohan Robinson <rohan.c.robinson.21@dartmouth.edu>
2025-03-19 17:51:27 -0500

    changes to make text visible on simulator , added ability to scroll on simulator

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,25 +1,26 @@
 import { router } from 'expo-router';
-import { Image, StyleSheet, TouchableOpacity } from 'react-native';
+import { Image, StyleSheet, ScrollView, TouchableOpacity } from 'react-native';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 
 export default function HomeScreen() {
   return (
+    <ScrollView contentContainerStyle={styles.scrollContainer}>
       <ThemedView style={styles.container}>
             <Image 
               source={require('@/assets/images/skinglow-logo.png')} 
              />
-            <ThemedText type="title">Skin Glow</ThemedText>
-              <ThemedText type="subtitle">In 3 minutes you'll be put on the path to looking your best!</ThemedText> 
-              <ThemedText type="subtitle">Easy as <ThemedText style={{fontWeight: 'bold'}}>1-2-3</ThemedText></ThemedText>
+            <ThemedText style={styles.defaultText} type="title" >Skin Glow</ThemedText>
+              <ThemedText style={styles.defaultText} type="subtitle">In 3 minutes you'll be put on the path to looking your best!</ThemedText> 
+              <ThemedText style={styles.defaultText} type="subtitle">Easy as <ThemedText style={styles.defaultText}>1-2-3</ThemedText></ThemedText>
           <ThemedView style={styles.stepContainer}>
-            <ThemedText type="subtitle">(1) Take some Pictures</ThemedText>
+            <ThemedText  style={styles.defaultText} type="subtitle">(1) Take some Pictures</ThemedText>
           </ThemedView> 
            <ThemedView style={styles.stepContainer}>
-            <ThemedText type="subtitle">(2) Answer a Few Questions </ThemedText>
+            <ThemedText  style={styles.defaultText} type="subtitle">(2) Answer a Few Questions </ThemedText>
           </ThemedView> 
            <ThemedView style={styles.stepContainer}>
-            <ThemedText type="subtitle"> (3) Checkout a Report and Routine Personalized for You!</ThemedText>
+            <ThemedText style={styles.defaultText}  type="subtitle"> (3) Checkout a Report and Routine Personalized for You!</ThemedText>
           </ThemedView> 
           <TouchableOpacity
             style={styles.button}
@@ -28,6 +29,7 @@ export default function HomeScreen() {
            <ThemedText style={styles.buttonText}>Start</ThemedText>
           </TouchableOpacity>
       </ThemedView>
+    </ScrollView>
   );
 }
 
@@ -36,6 +38,12 @@ const styles = StyleSheet.create({
     flex: 1, 
     backgroundColor: '#EFE0F2',
     alignItems: 'center', 
+  },
+  scrollContainer: {
+     flexGrow: 1,
+  },
+  defaultText: {
+    color: 'black',
   },
   titleContainer: {
     flexDirection: 'row',

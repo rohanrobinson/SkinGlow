3df0f780d332ea9aafbe5325c2b306b3c4326041
Rohan Robinson <rohan.c.robinson.21@dartmouth.edu>
2025-04-07 19:14:17 -0500

    test change

--- app/(tabs)/user-info.tsx
+++ app/(tabs)/user-info.tsx
@@ -26,7 +26,7 @@ export default function UserInfoScreen() {
         source={require('@/assets/images/skinglow-logo.png')} 
       />
     </TouchableOpacity>
-      <ThemedText style={styles.defaultText}>Help Us Get to Know You!</ThemedText>
+      <ThemedText style={styles.defaultText}>Help Us Get to Know You! TEST</ThemedText>
 
       <ThemedText style={styles.defaultText}>Name</ThemedText>
       <TextInput
@@ -53,7 +53,7 @@ export default function UserInfoScreen() {
       />
 
       <TouchableOpacity
-            onPress={() => router.push('/user-info') }
+            onPress={() => router.push('/') }
       >
            <ThemedText style={styles.buttonText}>&larr;</ThemedText> 
 
